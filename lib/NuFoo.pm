ce93b76194d583cda2481ccd7101d77acf9f42bc
Mark Pitchless <markpitchless@gmail.com>
2009-10-04 11:14:28 -0500

    Basic module skeleton created with module-starter.

---
+++ lib/NuFoo.pm
@@ -0,0 +1,88 @@
+package NuFoo;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+NuFoo - Developer tool for creating new boilerplace files using a set of builder modules.
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+This is the internal library for NuFoo, you probably want the command line
+interface, L<nufoo>:
+
+ $ nufoo --help
+ $ nufoo Perl.Moose.Class --name='My::Foo'
+
+=head1 DESCRIPTION
+
+=head1 METHODS 
+
+=head1 AUTHOR
+
+Mark Pitchless, C<< <markpitchless at gmail.com> >>
+
+=head1 BUGS
+
+All complex software has bugs lurking in it, and this module is no exception.
+
+Please report any bugs or feature requests to C<bug-nufoo at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=NuFoo>.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc NuFoo
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=NuFoo>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/NuFoo>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/NuFoo>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/NuFoo/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Mark Pitchless.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1; # End of NuFoo
