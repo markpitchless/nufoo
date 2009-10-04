e3aaccb53873ba8bbbb998e0daad8b6fa1d7784b
Mark Pitchless <markpitchless@gmail.com>
2009-10-04 12:51:40 -0500

    Added Builder base class skeleton.

--- lib/NuFoo/Builder.pm
+++ lib/NuFoo/Builder.pm
@@ -1,8 +1,8 @@
-package NuFoo::Cmd;
+package NuFoo::Builder;
 
 =head1 NAME
 
-NuFoo::Cmd - Impliments the command line interface to NuFoo. 
+NuFoo::Builder - Base class for builders. 
 
 =head1 VERSION
 
@@ -15,28 +15,15 @@ use 5.010;
 use Moose;
 use MooseX::Method::Signatures;
 
-extends 'NuFoo';
-
 with 'MooseX::Getopt';
 
-# This activates use of Getopt::Long::Descriptive for usage (along with
-# _usage_format below) to give the --help option.
-# It is a bit restrictive, so should move to Pod::Usage at some point.
-has help => ( is => 'rw', isa => 'Bool',
-    documentation => "Display help message" );
-
-sub _usage_format {
-    return "usage: %c [OPTIONS] [BUILDER [BUILDER_OPTIONS]]";
-}
-
 before new_with_options => sub {
-    Getopt::Long::Configure('pass_through'); 
+    Getopt::Long::Configure('no_pass_through'); 
 };
 
-method run() {
-    my @argv = @{$self->extra_argv};
-    my $name = shift @argv;
-    die "No builder name" if !$name || $name =~ m/^-/;
+method build() {
+    my $class = blessed $self;
+    confess "method build is abstract, $class must impliment";
 }
 
 1;
@@ -44,17 +31,16 @@ __END__
 
 =head1 SYNOPSIS
 
- #!/usr/bin/perl
- my $nufoo = NuFoo::Cmd->new_with_options;
- $nufoo->run;
-
 =head1 DESCRIPTION
+
+A builder does the actual work of generating files for the user. This is the
+base class all builders must be based on.
 
 =head1 METHODS 
 
-=head2 run
+=head2 build 
 
-Run the command line.
+Sub classers must impliment with their build logic.
 
 =head1 SEE ALSO
 
--- t/00-load.t
+++ t/00-load.t
@@ -1,10 +1,11 @@
 #!perl -T
 
-use Test::More tests => 2;
+use Test::More tests => 3;
 
 BEGIN {
     use_ok( 'NuFoo' );
     use_ok( 'NuFoo::Cmd' );
+    use_ok( 'NuFoo::Builder' );
 }
 
 diag( "Testing NuFoo $NuFoo::VERSION, Perl $], $^X" );
