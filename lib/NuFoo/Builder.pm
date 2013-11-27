7a90890951649dc4e23b46439fe557636d886a98
Mark Pitchless <markpitchless@gmail.com>
2013-11-27 20:32:48 +0000

    Start converting NuFoo::Builder to Moops.

--- lib/NuFoo/Builder.pm
+++ lib/NuFoo/Builder.pm
@@ -1,4 +1,5 @@
 package NuFoo::Builder;
+use Moops;
 
 =head1 NAME
 
@@ -10,110 +11,104 @@ Version 0.01
 
 =cut
 
-our $VERSION = '0.01';
-use 5.010;
-use Moose;
-use MooseX::Method::Signatures;
-use MooseX::StrictConstructor;
-use Log::Any qw($log);
-use File::Spec::Functions qw(rel2abs splitpath catdir);
-use NuFoo;
+class NuFoo::Builder 0.01
+    with MooseX::Getopt::Usage, MooseX::Getopt::Usage::Role::Man
+    using Moose {
+    use Log::Any qw($log);
+    use File::Spec::Functions qw(rel2abs splitpath catdir);
+    use NuFoo;
 
-with 'MooseX::Getopt::Usage',
-     'MooseX::Getopt::Usage::Role::Man';
+    has nufoo => (
+        is       => "ro",
+        isa      => "NuFoo",
+        required => 1,
+        traits   => ['NoGetopt'],
+        handles  => [qw(write_file)],
+    )
 
-has nufoo => (
-    is       => "ro",
-    isa      => "NuFoo",
-    required => 1,
-    traits   => ['NoGetopt'],
-    handles  => [qw(write_file)],
-);
+    around BUILDARGS ( $next: $class, @args ) {
+        my %args = ref $args[0] eq 'HASH' ? %{$args[0]} : @args;
 
-around BUILDARGS => sub {
-    my $orig  = shift;
-    my $class = shift;
-    my %args  = ref $_[0] eq 'HASH' ? %{$_[0]} : @_;
-
-    # Load config from the NuFoo object
-    # Searches in role namespace and then builder name
-    if ($args{nufoo}) {
-        my $conf     = $args{nufoo}->conf;
-        my @sections = $class->build_name;
-        foreach ( $class->meta->calculate_all_roles_with_inheritance ) {
-            my $name = $_->name;
-            next if $name =~ /\|/; # wtf are these piped name lists?
-            $name =~ s/^NuFoo::Role::// || next;
-            $name =~ s/::/\./g;
-            unshift @sections, $name;
-        }
-        my %extra;
-        foreach my $section (@sections) {
-            foreach ( $class->build_attribs ) {
-                my $name = $section . "." . $_->name;
-                my $val  = $conf->get($name);
-                $extra{$_->name} = $val if defined $val;
+        # Load config from the NuFoo object
+        # Searches in role namespace and then builder name
+        if ($args{nufoo}) {
+            my $conf     = $args{nufoo}->conf;
+            my @sections = $class->build_name;
+            foreach ( $class->meta->calculate_all_roles_with_inheritance ) {
+                my $name = $_->name;
+                next if $name =~ /\|/; # wtf are these piped name lists?
+                $name =~ s/^NuFoo::Role::// || next;
+                $name =~ s/::/\./g;
+                unshift @sections, $name;
             }
+            my %extra;
+            foreach my $section (@sections) {
+                foreach ( $class->build_attribs ) {
+                    my $name = $section . "." . $_->name;
+                    my $val  = $conf->get($name);
+                    $extra{$_->name} = $val if defined $val;
+                }
+            }
+            # Note that args (command line, new) overrides extra (conf file)
+            %args = ( %extra, %args ) if keys %extra;
         }
-        # Note that args (command line, new) overrides extra (conf file)
-        %args = ( %extra, %args ) if keys %extra;
+
+        return $class->${^NEXT}(%args);
     }
 
-    return $class->$orig(%args);
-};
+    # Show the builder name in the usage.
+    # Note: If a builder defines a SYNOPSIS in its POD that will override.
+    sub getopt_usage_config {
+        my $proto = shift;
+        my $class = ref $proto || $proto;
+        my $name  = NuFoo->builder_class_to_name($class);
+        return ( format => "%c $name %r [OPTIONS]" );
+    }
 
-# Show the builder name in the usage.
-# Note: If a builder defines a SYNOPSIS in its POD that will override.
-sub getopt_usage_config {
-    my $proto = shift;
-    my $class = ref $proto || $proto;
-    my $name  = NuFoo->builder_class_to_name($class);
-    return ( format => "%c $name %r [OPTIONS]" );
-}
+    method home_dir($self:) {
+        my $class = blessed $self || $self;
+        (my $inc_file = $class) =~ s/::/\//g;
+        $inc_file .= ".pm";
+        my $file  = $INC{$inc_file} || return undef;
+        my (undef, $dir, $name) = splitpath($file);
+        $name =~ s/\.pm$//;
+        my $home_dir = catdir($dir, $name);
+        return rel2abs($home_dir);
+    }
 
-method home_dir($self:) {
-    my $class = blessed $self || $self;
-    (my $inc_file = $class) =~ s/::/\//g;
-    $inc_file .= ".pm";
-    my $file  = $INC{$inc_file} || return undef; 
-    my (undef, $dir, $name) = splitpath($file);
-    $name =~ s/\.pm$//;
-    my $home_dir = catdir($dir, $name);
-    return rel2abs($home_dir);
-}
+    method build() {
+        my $class = blessed $self;
+        confess "method build is abstract, $class must impliment";
+    }
 
-method build() {
-    my $class = blessed $self;
-    confess "method build is abstract, $class must impliment";
-}
+    sub build_name {
+        my $class = shift;
+        $class = blessed $class || $class;
+        return NuFoo->builder_class_to_name($class);
+    }
 
-sub build_name {
-    my $class = shift;
-    $class = blessed $class || $class;
-    return NuFoo->builder_class_to_name($class);
-}
+    sub build_attribs {
+        my $class = shift;
+        grep {
+            # Ready for when we add a specific trait for Build attributes.
+            #$_->does("NuFoo::Meta::Attribute::Trait::Build")
+            #    or
+            $_->name !~ /^_/
+        } grep {
+            !$_->does('NuFoo::Meta::Attribute::Trait::NoBuild')
+        } $class->meta->get_all_attributes
+    }
 
-sub build_attribs {
-    my $class = shift;
-    grep {
-        # Ready for when we add a specific trait for Build attributes.
-        #$_->does("NuFoo::Meta::Attribute::Trait::Build")
-        #    or
-        $_->name !~ /^_/
-    } grep {
-        !$_->does('NuFoo::Meta::Attribute::Trait::NoBuild')
-    } $class->meta->get_all_attributes
-}
-
-sub build_vars {
-    my $self = shift;
-    my $vars = {};
-    foreach my $attr ($self->build_attribs) {
-        my $name = $attr->name;
-        my $meth = $attr->get_read_method;
-        $vars->{$name} = $self->$meth;
-    } 
-    return $vars;
+    sub build_vars {
+        my $self = shift;
+        my $vars = {};
+        foreach my $attr ($self->build_attribs) {
+            my $name = $attr->name;
+            my $meth = $attr->get_read_method;
+            $vars->{$name} = $self->$meth;
+        }
+        return $vars;
+    }
 }
 
 1;
